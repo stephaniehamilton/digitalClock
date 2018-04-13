8626c9fbd18c4ec0d6502e1ec33a1b69e943ea80
stephaniehamilton <smhamilton0208@gmail.com>
2018-04-12 22:36:50 -0400

    Simple digital clock tutorial from 17 projects in C#

---
+++ digitalClock/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace digitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.hourMinuteLabel = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.weekdayLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // hourMinuteLabel
+            // 
+            this.hourMinuteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hourMinuteLabel.ForeColor = System.Drawing.Color.White;
+            this.hourMinuteLabel.Location = new System.Drawing.Point(152, 105);
+            this.hourMinuteLabel.Name = "hourMinuteLabel";
+            this.hourMinuteLabel.Size = new System.Drawing.Size(249, 90);
+            this.hourMinuteLabel.TabIndex = 0;
+            this.hourMinuteLabel.Text = "19 : 25";
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsLabel.ForeColor = System.Drawing.Color.White;
+            this.secondsLabel.Location = new System.Drawing.Point(441, 105);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(117, 90);
+            this.secondsLabel.TabIndex = 1;
+            this.secondsLabel.Text = "47";
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateLabel.ForeColor = System.Drawing.Color.White;
+            this.dateLabel.Location = new System.Drawing.Point(152, 207);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(407, 97);
+            this.dateLabel.TabIndex = 2;
+            this.dateLabel.Text = "Jan 03 2018";
+            // 
+            // weekdayLabel
+            // 
+            this.weekdayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weekdayLabel.ForeColor = System.Drawing.Color.White;
+            this.weekdayLabel.Location = new System.Drawing.Point(152, 304);
+            this.weekdayLabel.Name = "weekdayLabel";
+            this.weekdayLabel.Size = new System.Drawing.Size(407, 90);
+            this.weekdayLabel.TabIndex = 3;
+            this.weekdayLabel.Text = "Wednesday";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(856, 423);
+            this.Controls.Add(this.weekdayLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.secondsLabel);
+            this.Controls.Add(this.hourMinuteLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hourMinuteLabel;
+        private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label weekdayLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
